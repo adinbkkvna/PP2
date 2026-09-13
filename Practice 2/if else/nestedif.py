5bc13ac39db0de9c17bc38d962f2fe73e7247efd
Adina <adina_kuralbek1@mail.ru>
2026-09-13 22:07:54 +0500

    nested if

---
+++ Practice 2/if else/nestedif.py
@@ -0,0 +1,69 @@
+age = 25
+has_license = True
+
+if age >= 18:
+  if has_license:
+    print("You can drive")
+  else:
+    print("You need a license")
+else:
+  print("You are too young to drive")
+
+
+score = 85
+attendance = 90
+submitted = True
+if score >= 60:
+  if attendance >= 80:
+    if submitted:
+      print("Pass with good standing")
+    else:
+      print("Pass but missing assignment")
+  else:
+    print("Pass but low attendance")
+else:
+  print("Fail")
+
+
+temperature = 25
+is_sunny = True
+if temperature > 20:
+  if is_sunny:
+    print("Perfect beach weather!")
+
+temperature = 25
+is_sunny = True
+
+if temperature > 20 and is_sunny:
+  print("Perfect beach weather!")
+
+
+username = "Emil"
+password = "python123"
+is_active = True
+
+if username:
+  if password:
+    if is_active:
+      print("Login successful")
+    else:
+      print("Account is not active")
+  else:
+    print("Password required")
+else:
+  print("Username required")
+
+
+
+score = 92
+extra_credit = 5
+
+if score >= 90:
+  if extra_credit > 0:
+    print("A+ grade")
+  else:
+    print("A grade")
+elif score >= 80:
+  print("B grade")
+else:
+  print("C grade or below")
