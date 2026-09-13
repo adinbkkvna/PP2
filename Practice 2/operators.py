1c1868e04c7c6dce51e47932abd7b26bb4a28992
Adina <adina_kuralbek1@mail.ru>
2026-09-13 21:49:44 +0500

    operators python

---
+++ Practice 2/operators.py
@@ -0,0 +1,20 @@
+print(10 + 5)
+
+
+sum1 = 100 + 50      # 150 (100 + 50)
+sum2 = sum1 + 250    # 400 (150 + 250)
+sum3 = sum2 + sum2   # 800 (400 + 400)
+
+num = 6
+x = "WEEKEND!" if num > 5 else "Workday"
+print(x)
+
+x = 5
+print(x > 0 and x < 10)
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+print(x is z)
+print(x is y)
+print(x == y)
+
