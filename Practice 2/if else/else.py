5390f1f5b87480de0a5966d6da14c1b88202ab55
Adina <adina_kuralbek1@mail.ru>
2026-09-13 22:35:07 +0500

    if else elif

--- Practice 2/if else/else.py
+++ Practice 2/if else/else.py
@@ -1,11 +1,3 @@
-a = 200
-b = 33
-if b > a:
-  print("b is greater than a")
-elif a == b:
-  print("a and b are equal")
-else:
-  print("a is greater than b")
 
 a = 200
 b = 33
@@ -21,13 +13,3 @@ else:
   print("The number is odd")
 
 
-temperature = 22
-
-if temperature > 30:
-  print("It's hot outside!")
-elif temperature > 20:
-  print("It's warm outside")
-elif temperature > 10:
-  print("It's cool outside")
-else:
-  print("It's cold outside!")
